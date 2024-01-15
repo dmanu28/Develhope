7f459945e566f0653af9c4170194b571c43e13b1
D. Manu <78975450+dmanu28@users.noreply.github.com>
2024-01-15 19:45:24 +0100

    Update Forma.java

--- polimorf/Forma.java
+++ polimorf/Forma.java
@@ -1,43 +1,13 @@
 package polimorf;
 
 public class Forma {
-	private double base;
-	private double altezza;
+	
 	private double area;
 	
-	public Forma(double base, double altezza) {
-		super();
-		this.base = base;
-		this.altezza = altezza;
+	public Forma() {
 	}
 
-	public double getBase() {
-		return base;
-	}
-
-	public void setBase(double base) {
-		this.base = base;
-	}
-
-	public double getAltezza() {
-		return altezza;
-	}
-
-	public void setAltezza(double altezza) {
-		this.altezza = altezza;
-	}
-
-	public double getArea() {
-		return area;
-	}
-
-	public void setArea(double area) {
-		this.area = area;
-	} 
-	
 	public void calcolaArea() {
-		this.setArea(this.base * this.altezza);
-		System.out.println(this.getArea());
 	}
 
 }
