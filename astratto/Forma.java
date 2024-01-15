e96205807751f2eedc083f4b7a4ee898fba730a0
D. Manu <78975450+dmanu28@users.noreply.github.com>
2024-01-15 19:41:04 +0100

    Update Forma.java

--- astratto/Forma.java
+++ astratto/Forma.java
@@ -2,47 +2,6 @@ package astratto;
 
 public abstract class Forma {
 	
-	private double base;
-	private double altezza;
-	private double area;
-	
-	
-	
-	public Forma(double base, double altezza) {
-		this.base = base;
-		this.altezza = altezza;
-	}
-
-
-	public double getBase() {
-		return base;
-	}
-
-
-	public void setBase(double base) {
-		this.base = base;
-	}
-
-
-	public double getAltezza() {
-		return altezza;
-	}
-
-
-	public void setAltezza(double altezza) {
-		this.altezza = altezza;
-	}
-
-
-	public double getArea() {
-		return area;
-	}
-
-
-	public void setArea(double area) {
-		this.area = area;
-	}
-
-
 	public abstract void calcolaArea(); 
 }
+
